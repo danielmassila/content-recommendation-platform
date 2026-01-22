af72b40be394da31fb68d51bba03d1f641b2a15f
Daniel Massila <daniel.massila-akendengue@insa-lyon.fr>
2026-01-22 12:09:00 +0100

    chore: initialise spring boot project

---
+++ reco/src/test/java/com/example/reco/RecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
