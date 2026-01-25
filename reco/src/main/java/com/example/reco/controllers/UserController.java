8a931efc451a2e7b7000816a55cbea7c5e6ece40
Daniel Massila <daniel.massila-akendengue@insa-lyon.fr>
2026-01-25 17:33:02 +0100

    feat: initiatialize user service and controller

---
+++ reco/src/main/java/com/example/reco/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.example.reco.controllers;
+
+import com.example.reco.services.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
---
+++ reco/src/main/java/com/example/reco/services/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.reco.services;
+
+public class UserServiceImpl {
+}
